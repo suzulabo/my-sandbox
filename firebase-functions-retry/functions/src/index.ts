6efc6275a3e7ef8c5ef61f0f576292d559222dde
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-01-02 01:11:08 +0900

    functions (WIP

--- firebase-functions-retry/functions/src/index.ts
+++ firebase-functions-retry/functions/src/index.ts
@@ -1,13 +1,19 @@
+import { PubSub } from "@google-cloud/pubsub";
 import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const addTask = functions.https.onRequest(async (_req, res) => {
+  functions.logger.info("addTask");
 
-export const addTask = functions.https.onRequest(() => {
-  //
+  const pubsub = new PubSub();
+  const topic = pubsub.topic("functions-retry");
+  await topic.publishMessage({ json: {} });
+
+  res.status(200).send("ok");
 });
+
+export const pubsubTask = functions.pubsub
+  .topic("functions-retry")
+  .onPublish(async () => {
+    functions.logger.info("pubsubTask");
+    return 0;
+  });
